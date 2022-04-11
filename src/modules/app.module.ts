bc5745271766a12b65a939ed9e1871bfe35c6465
kume1a <kumela011@gmail.com>
2022-04-11 23:02:32 +0400

    serve static uploaded files

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,15 +1,20 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { AuthenticationModule } from './modules/authentication/authentication,module';
+import { AuthenticationModule } from './authentication/authentication,module';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RolesGuard } from './guard/roles.guard';
-import { UserModule } from './modules/user/user.module';
-import { CategoryModule } from './modules/category/category.module';
-import { ProductModule } from './modules/product/product.module';
+import { RolesGuard } from '../guard/roles.guard';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'upload'),
+    }),
     AuthenticationModule,
     UserModule,
     CategoryModule,
